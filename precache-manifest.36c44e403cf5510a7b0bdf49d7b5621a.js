8220bf4839725a40772a1700e465aaeeb1631f0d
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-09-10 22:39:51 +0530

    Updates

---
+++ precache-manifest.36c44e403cf5510a7b0bdf49d7b5621a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1063ff25c8f8e1dcb55",
+    "url": "/ReactJs-TodoList/static/css/main.e9e92011.chunk.css"
+  },
+  {
+    "revision": "c1063ff25c8f8e1dcb55",
+    "url": "/ReactJs-TodoList/static/js/main.c1063ff2.chunk.js"
+  },
+  {
+    "revision": "23172ffde8cf25f62d55",
+    "url": "/ReactJs-TodoList/static/js/1.23172ffd.chunk.js"
+  },
+  {
+    "revision": "15778cb206573a761758",
+    "url": "/ReactJs-TodoList/static/css/2.a07d435b.chunk.css"
+  },
+  {
+    "revision": "15778cb206573a761758",
+    "url": "/ReactJs-TodoList/static/js/2.15778cb2.chunk.js"
+  },
+  {
+    "revision": "229686123779dd58e1ba",
+    "url": "/ReactJs-TodoList/static/js/runtime~main.22968612.js"
+  },
+  {
+    "revision": "3b71a7e6f9fd6b0dd4c0151c4de39b9a",
+    "url": "/ReactJs-TodoList/index.html"
+  }
+];
