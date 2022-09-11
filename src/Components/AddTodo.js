08a5a2f78e26027a6a84b8eeac639daec3e032df
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-09-11 20:23:26 +0530

    One issue fixed

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -5,42 +5,43 @@ import Form from "react-bootstrap/Form";
 const AddTodo = (props) => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
+
   const submit = (e) => {
     e.preventDefault();
     if (!title || !desc) {
       alert("Title and Description are madatory");
     } else {
+      props.addTodo(title, desc);
       setTitle("");
       setDesc("");
     }
-    props.addTodo(title, desc);
   };
   return (
-    <div className='container my-3'>
-      <h3 className='text-center'>Add Todo</h3>
+    <div className="container my-3">
+      <h3 className="text-center">Add Todo</h3>
       <Form onSubmit={submit}>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='disabledTextInput'>Title</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledTextInput">Title</Form.Label>
           <Form.Control
-            id='title'
+            id="title"
             value={title}
             onChange={(e) => {
               e.preventDefault();
               setTitle(e.target.value);
             }}
-            placeholder='Title'
+            placeholder="Title"
           />
         </Form.Group>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='disabledTextInput'>Description</Form.Label>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledTextInput">Description</Form.Label>
           <Form.Control
-            id='desc'
+            id="desc"
             value={desc}
             onChange={(e) => setDesc(e.target.value)}
-            placeholder='Description'
+            placeholder="Description"
           />
         </Form.Group>
-        <Button type='submit'>Add</Button>
+        <Button type="submit">Add</Button>
       </Form>
     </div>
   );
